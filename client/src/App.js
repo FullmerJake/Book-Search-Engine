f443d37334e3b24af13f1b83f5b57404532c07ab
Jake Fullmer <w.jake.fullmer@gmail.com>
2021-01-10 20:43:32 -0700

    added needed Apollo server logic to App.js in /src folder

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,22 @@ import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({ 
+  request: operation => {
+    const token = localStorage.getItem('id_token');
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}`: ''
+      }
+    })
+  },
+  uri: '/graphql'
+});
+
 function App() {
   return (
     <Router>
