b0565312ec188425c63962e41e08a29c090f0fcd
Jake Fullmer <w.jake.fullmer@gmail.com>
2021-01-10 20:55:48 -0700

    added queries.js and mutation.js logic in client/src/utils

---
+++ client/src/utils/queries.js
@@ -0,0 +1,18 @@
+import gql from "graphql-tag";
+
+export const GET_ME = gql`
+    me {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+            bookId
+            authors
+            title
+            description
+            image
+            link
+        }
+    }
+`
