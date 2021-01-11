21fdda8a3600a9d4f071e32960010ffd40606461
Jake Fullmer <w.jake.fullmer@gmail.com>
2021-01-10 20:34:00 -0700

    added resolver logic

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -19,19 +19,55 @@ const resolvers = {
 
     Mutation: {
         addUser: async (parent, ars) => {
+            const user = await User.create(args);
+            const token = signToken(user);
 
+            return {token, user};
         },
 
         login: async(parent, {email, password}) => {
+            const user = await User.findOne({email});
 
+            if(!user) {
+                throw new AuthenticationError('Wrong Username or Password!')
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw) {
+                throw new AuthenticationError('Wrong Username or Password!')
+            }
+
+            const token = signToken(user);
+            return {token, user};
         },
 
         saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id},
+                    { $addToSet: { savedBooks: args.input } },
+                    { new: true}
+                );
 
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('Must be logged in to do this!')
         },
 
         removeBook: async (parent, args, context) => {
+            if(context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: args.bookId } } },
+                    { new: true }
+                );
 
+                return updatedUser;
+            }
+
+            throw new AuthenticationError('Must be logged in do this!')
         }
     }
 };
